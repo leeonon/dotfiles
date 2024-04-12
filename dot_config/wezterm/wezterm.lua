991b4c1e1ec9e6c03b31d5636e367d93d1952dab
leeonon <v5sollee@gmail.com>
2024-04-13 01:07:28 +0800

    add wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font_size = 14.0
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" })
+config.color_scheme = "Catppuccin Mocha"
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+config.show_new_tab_button_in_tab_bar = false
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 70
+config.adjust_window_size_when_changing_font_size = false
+config.text_background_opacity = 0.9
+
+config.window_padding = {
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 5,
+}
+
+return config

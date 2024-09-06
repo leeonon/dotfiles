a6cbb6f2994b0cf763a02c4f38752a1c83221c9d
leeonon <v5sollee@gmail.com>
2024-09-06 14:18:19 +0800

    wezterm 配置修改

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,70 +4,70 @@ local k = require("utils/keys")
 local config = wezterm.config_builder()
 local act = wezterm.action
 
-config.font_size = 14
+config.font_size = 14.0
 config.font = wezterm.font_with_fallback({
-	{ family = "GeistMono Nerd Font Mono" },
-	"MartianMono Nerd Font Mono",
-	"JeBrainsMono Nerd Font Mono",
-	"ComicShannsMono Nerd Font Mono",
-	{ family = "BlexMono Nerd Font Mono", weight = "Medium" },
+	{ family = "BlexMono Nerd Font" },
+	-- { family = "Monaspace Neon" },
+	-- { family = "MonoLisa", weight = "Regular" },
+	-- { family = "Iosevka Nerd Font Mono" },
+	-- { family = "FantasqueSansM Nerd Font" },
+	-- { family = "JetBrainsMono Nerd Font Mono", scale = 1.0, weight = "Medium" },
+	-- { family = "IBM Plex Mono" },
+	-- { family = "GeistMono Nerd Font", weight = "Medium" },
+	-- { family = "ComicShannsMono Nerd Font Mono", scale = 1.2, weight = "Medium" },
+	-- "MartianMono Nerd Font Mono",
+	-- "ComicShannsMono Nerd Font Mono",
 })
--- config.font_rules = {
--- 	{
--- 		intensity = "Bold",
--- 		italic = true,
--- 		font = wezterm.font({
--- 			family = "BlexMono Nerd Font Mono",
--- 			weight = "Bold",
--- 			style = "Italic",
--- 		}),
--- 	},
--- 	{
--- 		italic = true,
--- 		intensity = "Half",
--- 		font = wezterm.font({
--- 			family = "BlexMono Nerd Font Mono",
--- 			weight = "DemiBold",
--- 			style = "Italic",
--- 		}),
--- 	},
--- 	{
--- 		italic = true,
--- 		intensity = "Normal",
--- 		font = wezterm.font({
--- 			family = "BlexMono Nerd Font Mono",
--- 			style = "Italic",
--- 		}),
--- 	},
--- }
 
+-- 字体 https://github.com/wez/wezterm/issues/3774
+-- config.front_end = 'WebGpu' -- WebGpu/OpenGL
+config.front_end = "WebGpu" -- WebGpu/OpenGL
+config.freetype_load_target = "Light"
+config.freetype_render_target = "HorizontalLcd"
+config.freetype_load_flags = "NO_HINTING"
+config.cell_width = 1.0
+
+config.color_scheme = "Tokyo Night"
+-- config.colors = require("themes/cyberdream")
+config.window_decorations = "RESIZE"
+
+config.window_close_confirmation = "AlwaysPrompt"
+config.scrollback_lines = 3000
+config.default_workspace = "main"
 config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
 config.show_new_tab_button_in_tab_bar = false
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 64
+
+-- config.window_background_image = "/Users/"
+-- 	.. os.getenv("USER")
+-- 	.. "/.local/share/chezmoi/dot_config/wezterm/images/Sand.jpg"
+
+config.background = {
+	{
+		source = {
+			File = "/Users/" .. os.getenv("USER") .. "/.local/share/chezmoi/dot_config/wezterm/images/Sand.jpg",
+		},
+		-- The texture tiles vertically but not horizontally.
+		-- When we repeat it, mirror it so that it appears "more seamless".
+		-- An alternative to this is to set `width = "100%"` and have
+		-- it stretch across the display
+		repeat_x = "Mirror",
+		opacity = 0.8,
+		hsb = { brightness = 0.5 },
+		-- When the viewport scrolls, move this layer 10% of the number of
+		-- pixels moved by the main viewport. This makes it appear to be
+		-- further behind the text.
+		attachment = { Parallax = 0.1 },
+	},
+}
 config.adjust_window_size_when_changing_font_size = false
 config.term = "xterm-256color"
 config.default_cursor_style = "BlinkingBar"
 config.line_height = 1.2
-
--- custon theme
-local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
--- custom.background = "#021727"
-custom.background = "#000000"
-custom.tab_bar.background = "#040404"
-custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
-custom.tab_bar.new_tab.bg_color = "#080808"
-config.color_schemes = {
-	["Custom Catppuccin Mocha"] = custom,
-}
-config.color_scheme = "Custom Catppuccin Mocha"
-
--- 设置 wezterm 窗口大小
-config.initial_rows = 45
-config.initial_cols = 150
-
+-- config.initial_rows = 38
+-- config.initial_cols = 155
 config.window_padding = {
 	left = 0,
 	right = 0,
